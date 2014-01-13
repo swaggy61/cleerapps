301b0bfcdd92bc30b766f31c1c69d127b8cd6c4b
Cleer Apps <paul.dossor@gmail.com>
2014-01-13 14:42:40 +1100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClickfindApp::Application.config.secret_key_base = 'd84d8dcf69e225fcca6ebf096096e260bf78d3134f49bbe6117daec1ca81b7dcb76b50172c3631ee167a4d8a8a92ad68807842f274752ba0c2d8e72b21c72f38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClickfindApp::Application.config.session_store :cookie_store, key: '_clickfind_app_session'
